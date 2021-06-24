16258c422c39e29fda29f1c7b1db91d301359793
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2021-06-24 09:54:36 -0300

    Updating Query Layer and DbQueryBase

--- UzunTec.Utils.DatabaseAbstraction/QueryExecutionLayer.cs
+++ UzunTec.Utils.DatabaseAbstraction/QueryExecutionLayer.cs
@@ -8,36 +8,42 @@ namespace UzunTec.Utils.DatabaseAbstraction
     {
         private readonly AbstractionOptions options;
         private readonly QueryPreProccess queryPreProcess;
+        private readonly ConnectionBuilder connectionBuilder;
 
-        public QueryExecutionLayer(AbstractionOptions options, QueryPreProccess queryPreProcess)
+        public QueryExecutionLayer(AbstractionOptions options, QueryPreProccess queryPreProcess, ConnectionBuilder connectionBuilder)
         {
             this.options = options;
             this.queryPreProcess = queryPreProcess;
+            this.connectionBuilder = connectionBuilder;
         }
 
-        public T SafeRunQuery<T>(IDbConnection conn, string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc) where T : class
+        public T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc) where T : class
         {
-            return this.SafeRunQuery<T>(conn, queryString, parameters, executionFunc, this.options.AutoCloseConnection);
+            return this.SafeRunQuery<T>(queryString, parameters, executionFunc, this.options.AutoCloseConnection);
         }
 
-        public T SafeRunQuery<T>(IDbConnection conn, string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc, bool closeConnection) where T : class
+        public T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc, bool closeConnection) where T : class
         {
             T output = null;
-            if (conn.State == ConnectionState.Closed)
-            {
-                conn.Open();
-            }
 
-            using (IDbCommand command = conn.CreateCommand(queryString, this.queryPreProcess.PreProcessParameters(queryString, parameters)))
+            this.connectionBuilder.OpenConnection(delegate (IDbConnection conn)
             {
-                command.CommandText = this.queryPreProcess.PreProcessQuery(command.CommandText);
-                output = executionFunc(command);
-            }
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
 
-            if (closeConnection)
-            {
-                conn.Close();
-            }
+                using (IDbCommand command = conn.CreateCommand(queryString, this.queryPreProcess.PreProcessParameters(queryString, parameters)))
+                {
+                    command.CommandText = this.queryPreProcess.PreProcessQuery(command.CommandText);
+                    output = executionFunc(command);
+                }
+
+                if (closeConnection)
+                {
+                    conn.Close();
+                }
+            });
 
             return output;
         }
