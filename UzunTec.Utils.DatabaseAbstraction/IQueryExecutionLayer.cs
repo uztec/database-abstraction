863a74dcb659791bc85e78aad60db32c10f35dd0
Renato Gobet Uzun <renato.uzun@uztec.com.br>
2024-09-03 22:43:49 -0300

    feat: Adding Async Methods

--- UzunTec.Utils.DatabaseAbstraction/IQueryExecutionLayer.cs
+++ UzunTec.Utils.DatabaseAbstraction/IQueryExecutionLayer.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
 
 namespace UzunTec.Utils.DatabaseAbstraction
 {
     public interface IQueryExecutionLayer
     {
-        T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc) where T : class;
-        T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc, bool closeConnection = false) where T : class;
+        T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, T> executionFunc) where T : class;
+        T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, T> executionFunc, bool closeConnection = false) where T : class;
+        Task<T> SafeRunQueryAsync<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, Task<T>> executionFunc) where T : class;
+        Task<T> SafeRunQueryAsync<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, Task<T>> executionFunc, bool closeConnection = false) where T : class;
+
     }
 }
--- UzunTec.Utils.DatabaseAbstraction/QueryExecutionLayerWithLock.cs
+++ UzunTec.Utils.DatabaseAbstraction/QueryExecutionLayerWithLock.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
 
 namespace UzunTec.Utils.DatabaseAbstraction
 {
@@ -14,7 +15,7 @@ namespace UzunTec.Utils.DatabaseAbstraction
             this.baseLayer = new QueryExecutionLayer(options, queryPreProcess, connectionBuilder);
         }
 
-        public T SafeRunQuery<T>( string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc) where T : class
+        public T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, T> executionFunc) where T : class
         {
             lock (this.queryLocking)
             {
@@ -22,11 +23,27 @@ namespace UzunTec.Utils.DatabaseAbstraction
             }
         }
 
-        public T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<IDbCommand, T> executionFunc, bool closeConnection) where T : class
+        public T SafeRunQuery<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, T> executionFunc, bool closeConnection) where T : class
         {
             lock (this.queryLocking)
             {
                 return this.baseLayer.SafeRunQuery<T>(queryString, parameters, executionFunc, closeConnection);
+            }
+        }
+
+        public Task<T> SafeRunQueryAsync<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, Task<T>> executionFunc) where T : class
+        {
+            lock (this.queryLocking)
+            {
+                return this.baseLayer.SafeRunQueryAsync<T>(queryString, parameters, executionFunc);
+            }
+        }
+
+        public Task<T> SafeRunQueryAsync<T>(string queryString, IEnumerable<DataBaseParameter> parameters, Func<DbCommand, Task<T>> executionFunc, bool closeConnection) where T : class
+        {
+            lock (this.queryLocking)
+            {
+                return this.baseLayer.SafeRunQueryAsync<T>(queryString, parameters, executionFunc, closeConnection);
             }
         }
     }
